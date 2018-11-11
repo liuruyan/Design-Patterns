67e88adc5e8c4dfa1f277b368483c92c63b0f767
liuruyan <1823192871@qq.com>
2018-11-12 06:15:34 +0800

    状态模式

---
+++ 状态模式/src/抽象状态类/State.java
@@ -0,0 +1,7 @@
+package 抽象状态类;
+
+public abstract class State {
+	
+	public abstract void WriteProgram(Work w);
+	
+}
---
+++ 状态模式/src/状态类/ForenoonState.java
@@ -0,0 +1,22 @@
+package 状态类;
+
+import 抽象状态类.State;
+import 抽象状态类.Work;
+
+public class ForenoonState extends State {
+
+	@Override
+	public void WriteProgram(Work w) {
+		// TODO Auto-generated method stub
+		
+		if(w.Hour<12) {
+			
+			System.out.println("当前时间"+);
+			
+		}
+		
+		
+		
+	}
+
+}
